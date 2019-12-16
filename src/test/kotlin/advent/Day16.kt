0d3513d1ea5518fbbd7d6c6fe6b0f155193716c5
Vincent FERRIES <vincent.ferries@gmail.com>
2019-12-16 08:20:40 +0100

    day16

---
+++ src/test/kotlin/advent/Day16.kt
@@ -0,0 +1,20 @@
+package advent
+
+import org.junit.jupiter.api.Test
+
+class Day16 {
+    @Test
+    fun `with input 12345678`() {
+        val input = "12345678".map(Char::toString).map(String::toInt)
+        val pattern = listOf(0, 1, 0, -1)
+        val result = input.mapIndexed { i, x ->
+            //TODO duplicate pattern ? appli round on division ?
+            lastDigit(input.mapIndexed { index, e ->
+                e * pattern[(index+1) % pattern.size]
+            }.sum())
+        }
+        println(result)
+    }
+
+    private fun lastDigit(i: Int): Int = i.toString().last().toString().toInt()
+}
